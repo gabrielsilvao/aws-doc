913e232da07c6bc3e8b4f7969cb9133c2c65ea3c
Gabriel <gabrielsilva.oliv20@gmail.com>
2022-03-03 09:52:25 -0300

    kinesis

---
+++ kinesis/kinesis-data-stream.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# get the AWS CLI version
+aws --version
+
+# PRODUCER
+
+# CLI v2
+aws kinesis put-record --stream-name test --partition-key user1 --data "user signup" --cli-binary-format raw-in-base64-out
+
+# describe the stream
+aws kinesis describe-stream --stream-name test
+
+# consume some data
+aws kinesis get-shard-iterator --stream-name test --shard-id shardId-000000000000 --shard-iterator-type TRIM_HORIZON
+
+aws kinesis get-records --shard-iterator <>
