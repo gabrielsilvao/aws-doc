d4845b622b036d04523292daef29298dbea5da23
loktwo <loktwo@outlook.com>
2022-06-13 21:44:46 -0300

    packer v1

--- terraform/packer/instance.tf
+++ terraform/packer/instance.tf
@@ -1,7 +1,7 @@
-resource "aws_instance" "example" {
+resource "aws_instance" "packer-instance" {
     ami = "${var.AMI_ID}"
     instance_type = "t2.micro"
-    subnet_id = "${aws_subnet.main-public-1.id}"
-    vpc_security_group_ids = ["${aws_security_group.example-instance.id}"]
+    subnet_id = aws_subnet.packer-sub-one.id
+    vpc_security_group_ids = [aws_security_group.packer-instance.id]
     key_name = "aws_key_pair.ec2-key-pair"
 }
